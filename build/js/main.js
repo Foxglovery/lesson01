79a2dac61d19ec2ec8bb71a0bcbca2b378b68773
Foxglovery <foxglovery@gmail.com>
2024-04-18 11:56:39 -0500

    finished lesson 2

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,49 @@
 "use strict";
-let myName = 'Gabriel';
-let meaningOfLife;
-let isLoading;
-let album;
-myName = 'Lem';
-meaningOfLife = 42;
-isLoading = true;
-album = 'humhum';
-const sum = (a, b) => {
-    return a + b;
+let stringArr = ['one', 'hey', 'Gabe'];
+let guitars = ['Strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'John';
+stringArr.push('Ixion');
+console.log(stringArr);
+guitars[0] = 1984;
+guitars.unshift('Jim');
+console.log(guitars);
+let test = [];
+let bands = [];
+bands.push('Pixies');
+//Tuple is more strict than array
+let myTuple = ['Gabe', 36, true];
+let mixed = ['john', 2, false];
+myTuple[1] = 43;
+//Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+;
+let evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "Frogwash"],
 };
-let postId;
-let isActive;
-let re = /\w+/g;
+let JP = {
+    name: "Jimmy",
+    active: true,
+    albums: ["I", "II", "III"],
+};
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}!`;
+    }
+    return 'Hello';
+};
+console.log(greetGuitarist(evh));
+//Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,65 @@
-let myName: string = 'Gabriel'
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any;
+let stringArr = ['one', 'hey', 'Gabe']
+let guitars = ['Strat', 'Les Paul', 5150]
 
-myName = 'Lem'
-meaningOfLife = 42
-isLoading = true
-album = 'humhum'
+let mixedData = ['EVH', 1984, true]
+stringArr[0] = 'John'
+stringArr.push('Ixion')
+console.log(stringArr)
 
-const sum = (a: number, b: string) => {
-    return a + b
+
+guitars[0] = 1984
+guitars.unshift('Jim')
+console.log(guitars)
+
+let test = []
+let bands: string[] = []
+bands.push('Pixies')
+
+//Tuple is more strict than array
+let myTuple: [string, number, boolean] = ['Gabe', 36, true]
+
+let mixed = ['john', 2, false]
+
+myTuple[1] = 43
+
+//Objects
+let myObj: object
+myObj = []
+console.log(typeof myObj)
+
+interface Guitarist {
+    name?: string,
+    active: Boolean,
+    albums: (string | number)[],
+};
+
+let evh: Guitarist = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "Frogwash"],
+};
+let JP: Guitarist = {
+    name: "Jimmy",
+    active: true,
+    albums: ["I", "II", "III"],
+};
+
+const greetGuitarist = (guitarist: Guitarist) => {
+    if (guitarist.name) {
+
+        return `Hello ${guitarist.name.toUpperCase()}!`
+    }
+    return 'Hello'
 }
+console.log(greetGuitarist(evh))
 
-let postId: string | number
-let isActive: number | boolean
+//Enums
 
-let re: RegExp = /\w+/g
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A,
+}
+console.log(Grade.U)
