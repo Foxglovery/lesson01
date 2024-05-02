4ccf161cc858be36d24720e8f45f509fe3a6adc3
Foxglovery <foxglovery@gmail.com>
2024-05-02 10:31:13 -0500

    commit

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,30 @@
 "use strict";
+// Literal Types
+let myName;
+//can only be assigned once, like a constant
+let userName;
+userName = 'Amy';
+// functions
+const add = (a, b) => {
+    return a + b;
+};
+//functions that don't have a return
+//functions without return should be void type; void declaration can be omitted
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg('Hello!');
+logMsg(add(2, 3));
+logMsg(add(4, 3));
+// non arrow function
+let subtract = function (c, d) {
+    return c - d;
+};
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+//  optional parameters
+const addAll = (a, b, c) => {
+    return a + b + c;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-// Type Aliases
+// Type Aliases for basic types and functions
 type stringOrNumber = string | number
 
 type stringOrNumberArray = (stringOrNumber)[]
+// type definitions normally go up top !!!
+type mathFunction = (a: number, b: number) => number
+
+//for classes and extending those
+// interface mathFunction {
+
+//     (a: number, b: number): number
+// }
+
 
 type Guitarist = {
     name?: string,
@@ -10,3 +19,44 @@ type Guitarist = {
 }
 
 type UserId = stringOrNumber
+
+// Literal Types
+let myName: 'Gabe'
+//can only be assigned once, like a constant
+let userName: 'Gabe' | 'John' | "Amy"
+userName = 'Amy'
+
+// functions
+const add = (a: number, b: number): number => {
+    return a + b
+}
+
+//functions that don't have a return
+//functions without return should be void type; void declaration can be omitted
+const logMsg = (message: any): void => {
+    console.log(message)
+}
+
+logMsg('Hello!')
+logMsg(add(2, 3))
+logMsg(add(4, 3))
+
+// non arrow function
+let subtract = function (c: number, d: number): number {
+    return c - d
+}
+
+
+
+let multiply: mathFunction = function (c, d) {
+    return c * d
+}
+
+logMsg(multiply(2, 2))
+
+//  optional parameters
+
+const addAll = (a: number, b: number, c?: number): number => {
+    return a + b + c
+}
+
